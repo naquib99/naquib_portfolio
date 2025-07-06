ac1748ecc6ac791a17b95f8284f6f10a496b4aef
naquib <a.naquib5@gmail.com>
2025-07-06 15:41:22 +0800

    Tweak to publish - 2

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "naquib_portfolio"
+  base: "/naquib_portfolio"
 });
